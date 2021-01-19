3b44f932fbd2123f297306bb0a634072403ee198
Frans Engström <frans.engstrom@learnet.se>
2021-01-19 12:52:46 +0100

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace CarSimulator
 {
@@ -6,7 +7,13 @@ namespace CarSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+           
+
         }
     }
+
+
 }
